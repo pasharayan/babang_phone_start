5ba2b13c01ab0e19ddb489724070a4353d777b2a
pasharayan <pasha.rayan@gmail.com>
2015-10-25 07:51:52 +1100

    updated mvp

---
+++ client/templates/babanginputs/profile.js
@@ -0,0 +1,20 @@
+Template.profile.events({
+  'submit .new-student': function(e){
+    e.preventDefault();
+
+    var data = {
+      name: $(e.target).find('[name=name]').val(),
+      sekolah: $(e.target).find('[name=sekolah]').val(),
+      thumbnailUrl: $(e.target).find('[name=thumbnailUrl]').val(),
+      teacherComment: $(e.target).find('[name=teacher]').val(),
+      previousAchievements: $(e.target).find('[name=previousachievements]').val(),
+      latitude: Number($(e.target).find('[name=latitude]').val()),
+      longitude: Number($(e.target).find('[name=longitude]').val()),
+    };
+
+    //console.log(data);
+    var studentId = Students.insert(data);
+    Router.go('homepage');
+
+  }
+});
---
+++ client/templates/babanginputs/project.js
@@ -0,0 +1,20 @@
+Template.project.events({
+  'submit .new-project': function(e){
+    e.preventDefault();
+
+    var data = {
+      name: $(e.target).find('[name=name]').val(),
+      thumbnailUrl: $(e.target).find('[name=thumbnailUrl]').val(),
+      tagline: $(e.target).find('[name=tagline]').val(),
+      story: $(e.target).find('[name=story]').val(),
+      latitude: Number($(e.target).find('[name=latitude]').val()),
+      longitude: Number($(e.target).find('[name=longitude]').val()),
+      goal: $(e.target).find('[name=goal]').val(),
+    };
+
+
+    var projectId = Projects.insert(data);
+    Router.go('homepage');
+
+  }
+});
